2b90ea02fc407ef0fb903347d0c9f337473736f1
Anmol <88301047+The-Anmol@users.noreply.github.com>
2022-07-26 20:26:05 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
+  <Helmet>
+        <title>Calculator in React</title>
+  </Helmet>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </>
 );
